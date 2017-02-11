1436629cb3fe4f6847b1aaee86050f5bb71b0aa5
Jordan Suchow <suchow@post.harvard.edu>
2017-02-11 14:25:54 -0500

    Improve submission button

--- static/scripts/questionnaire.js
+++ static/scripts/questionnaire.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-    //
+    // Submit the questionnaire.
+    $("#submit-questionnaire").click(function() {
+        console.log("Submitting questionnaire.");
+        submitResponses();
+    });
 });
