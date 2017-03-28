3383b3641a8c3e14676bac53c97ef8a9f0fd862a
Jesse Snyder <jsnyder@wesleyan.edu>
2017-03-28 10:44:27 -0700

    Update bundle.js

--- static/scripts/bundle.js
+++ static/scripts/bundle.js
@@ -22906,7 +22906,7 @@ var library = {
   }
 };
 
-Food = function(settings) {
+var Food = function(settings) {
   if (!(this instanceof Food)) {
     return new Food();
   }
@@ -23075,13 +23075,13 @@ pixels.frame(function() {
   // Add the Gaussian mask.
   limitVisibility = settings.visibility <
     Math.max(settings.columns, settings.rows);
-  if (limitVisibility && typeof ego !== "undefined") {
+  if (limitVisibility && typeof ego_index !== "undefined") {
     var g = gaussian(0, Math.pow(settings.visibility, 2));
     rescaling = 1 / g.pdf(0);
     for (var i = 0; i < settings.columns; i++) {
       for (var j = 0; j < settings.rows; j++) {
-        x = players[ego].position[0];
-        y = players[ego].position[1];
+        x = players[ego_index].position[0];
+        y = players[ego_index].position[1];
         dimness = g.pdf(distance(x, y, i, j)) * rescaling;
         idx = i * settings.columns + j;
         data[idx] = [
@@ -23127,6 +23127,7 @@ function openSocket(endpoint) {
 }
 
 $(document).ready(function() {
+  var player_id = getUrlParameter('participant_id');
   // Append the canvas.
   $("#grid").append(pixels.canvas);
 
@@ -23189,93 +23190,55 @@ $(document).ready(function() {
     donateToClicked(settings.donation);
   });
 
-  donateToClicked = function(amt) {
-    row = pixels2cells(mouse[1]);
-    column = pixels2cells(mouse[0]);
-    player_to = nearestPlayer(row, column);
-    if (player_to.id !== ego) {
+  var donateToClicked = function(amt) {
+    var row = pixels2cells(mouse[1]),
+        column = pixels2cells(mouse[0]),
+        recipient_index = nearestPlayerIndex(row, column),
+        recipient = players[recipient_index],
+        donor = players[ego_index],
+        msg;
+
+    if (recipient.id !== donor.id) {
       msg = {
-        type: "donate", 
-        player_to: player_to.id,
-        player_from: ego,
+        type: "donation_submitted", 
+        recipient_index: recipient_index,
+        donor_index: ego_index,
         amount: amt
       };
       sendToBackend(msg);
     }
   };
 
-  pixels2cells = function(pix) {
+  var pixels2cells = function(pix) {
     return Math.floor(pix / (settings.block_size + settings.padding));
   };
 
-  nearestPlayer = function(row, column) {
-    distances = [];
+  var nearestPlayerIndex = function(row, column) {
+    var distances = [];
     for (var i = 0; i < players.length; i++) {
       distances.push(
         Math.abs(row - players[i].position[0]) +
           Math.abs(column - players[i].position[1])
       );
     }
-    return players[distances.indexOf(Math.min.apply(null, distances))];
+    return distances.indexOf(Math.min.apply(null, distances));
   };
 
-
-  var inbox = openSocket('receive_chat');
-  var outbox = openSocket('send_chat');
-
-  outbox.onopen = function (event) {
-    data = {
-      type: 'connect',
-      player_id: getUrlParameter('participant_id'),
-    };
-    sendToBackend(data);
-  };
-
-  inbox.onmessage = function (event) {
-    if (event.data.indexOf(CHANNEL_MARKER) !== 0) { 
-      console.log("Message was not of our " + CHANNEL_MARKER + " channel. Ignoring");
-      return; 
-    }
-    var msg = JSON.parse(event.data.substring(CHANNEL_MARKER.length));
-    switch(msg.type) {
-      case "chat":
-        onChatMessage(msg);
-        break;
-      case "donate":
-        onDonate(msg);
-        break;
-      case "state":
-        onGameStateChange(msg);
-        break;
-      case "stop":
-        gameOver(msg);
-        break;
-      default:
-        console.log("Unrecognized message type " + msg.type + ' from backend.');
-    }
-  };
-
-  function sendToBackend(data) {
-    var msg = JSON.stringify(data);
-    console.log("Sending message to the backend: " + msg);
-    outbox.send(msg);
-  }
-  
-  onChatMessage = function (msg) {
+  var onChatMessage = function (msg) {
     var name;
     if (settings.pseudonyms) {
-      name = players[msg.player_id].name;
+      name = players[msg.player_index].name;
     } else {
-      name = "Player " + msg.player_id;
+      name = "Player " + msg.player_index;
     }
     entry = "<span class='name'>" + name + ":</span> " + msg.contents;
     $("#messages").append($("<li>").html(entry));
     $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
   };
 
-  onDonate = function (msg) {
-    entry = "Player " + msg.player_from + " gave you " + msg.donation;
-    if (msg.donation === 1) {
+  var onDonationProcessed = function (msg) {
+    entry = "Player " + msg.donor_index + " gave you " + msg.amount;
+    if (msg.amount === 1) {
       entry += " point.";
     } else {
       entry += " points.";
@@ -23284,11 +23247,9 @@ $(document).ready(function() {
     $("#chatlog").scrollTop($("#chatlog")[0].scrollHeight);
   };
 
-  onGameStateChange = function (msg) {
+  var onGameStateChange = function (msg) {
     // Update ego.
-    clients = msg.clients;
-    // XXX 
-    ego = clients.indexOf(participant_id);
+    ego_index = msg.clients.indexOf(player_id);
 
     // Update remaining time.
     $("#time").html(Math.max(Math.round(msg.remaining_time), 0));
@@ -23329,8 +23290,8 @@ $(document).ready(function() {
     }
 
     // Update displayed score.
-    $("#score").html(Math.round(players[ego].score));
-    dollars = (players[ego].score * settings.dollars_per_point).toFixed(2);
+    $("#score").html(Math.round(players[ego_index].score));
+    dollars = (players[ego_index].score * settings.dollars_per_point).toFixed(2);
     $("#dollars").html(dollars);
   };
 
@@ -23342,11 +23303,54 @@ $(document).ready(function() {
     pixels.canvas.style.display = "none";
   }
 
+
+  var inbox = openSocket('receive_chat');
+  var outbox = openSocket('send_chat');
+
+  outbox.onopen = function (event) {
+    data = {
+      type: 'connect',
+      player_id: player_id,
+    };
+    sendToBackend(data);
+  };
+
+  inbox.onmessage = function (event) {
+    if (event.data.indexOf(CHANNEL_MARKER) !== 0) { 
+      console.log("Message was not on channel " + CHANNEL_MARKER + ". Ignoring.");
+      return; 
+    }
+    var msg = JSON.parse(event.data.substring(CHANNEL_MARKER.length));
+    switch(msg.type) {
+      case "chat":
+        onChatMessage(msg);
+        break;
+      case "donation_processed":
+        onDonationProcessed(msg);
+        break;
+      case "state":
+        onGameStateChange(msg);
+        break;
+      case "stop":
+        gameOver(msg);
+        break;
+      default:
+        console.log("Unrecognized message type " + msg.type + ' from backend.');
+    }
+  };
+
+  function sendToBackend(data) {
+    var msg = JSON.stringify(data);
+    console.log("Sending message to the backend: " + msg);
+    outbox.send(msg);
+  }
+  
+
   $("form").submit(function() {
     var msg = {
       type: 'chat',
       contents: $("#message").val(),
-      player_id: ego,
+      player_index: ego_index,
       timestamp: Date.now() - start
     };
     sendToBackend(msg);
@@ -23362,10 +23366,10 @@ $(document).ready(function() {
   directions.forEach(function(direction) {
     Mousetrap.bind(direction, function() {
       if (!lock) {
-        players[ego].move(direction);
+        players[ego_index].move(direction);
         msg = {
           type: "move",
-          player: players[ego].id,
+          player: players[ego_index].id,
           move: direction,
         };
         sendToBackend(msg);
@@ -23386,18 +23390,18 @@ $(document).ready(function() {
   Mousetrap.bind("space", function () {
     msg = {
       type: "plant_food", 
-      player: players[ego].id,
-      position: players[ego].position,
+      player: players[ego_index].id,
+      position: players[ego_index].position,
     };
     sendToBackend(msg);
   });
 
   function createBinding (key) {
     Mousetrap.bind(key[0].toLowerCase(), function () {
-      players[ego].color = PLAYER_COLORS[key];
+      players[ego_index].color = PLAYER_COLORS[key];
       msg = {
         type: "change_color",
-        player: players[ego].id, 
+        player: players[ego_index].id, 
         color: PLAYER_COLORS[key]
       };
       sendToBackend(msg);
