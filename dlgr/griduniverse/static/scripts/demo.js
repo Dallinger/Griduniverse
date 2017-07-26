e0872bc580e6d172ee7a69a8c5c12004f9153c31
Jesse Snyder <jsnyder@wesleyan.edu>
2017-07-26 14:37:17 -0700

    Clean up JS

--- dlgr/griduniverse/static/scripts/demo.js
+++ dlgr/griduniverse/static/scripts/demo.js
@@ -444,7 +444,7 @@ pixels.frame(function() {
     var newColor = animateColor(color);
     background[coordsToIdx(x, y, settings.columns)] = newColor;
     return newColor;
-  })
+  });
 
   for (i = 0; i < food.length; i++) {
     // Players digest the food.
@@ -507,9 +507,11 @@ function arraysEqual(arr1, arr2) {
 }
 
 function arraySearch(arr, val) {
-    for (var i = 0; i < arr.length; i++)
-        if (arraysEqual(arr[i], val))
-            return i;
+    for (var i = 0; i < arr.length; i++) {
+      if (arraysEqual(arr[i], val)) {
+        return i;
+      }
+    }
     return false;
 }
 
@@ -518,13 +520,14 @@ function getRandomInt(min, max) {
 }
 
 function getWindowPosition() {
-  var ego = players.ego();
-  var w = {
-    left: 0,
-    top: 0,
-    columns: settings.window_columns,
-    rows: settings.window_rows
-  }
+  var ego = players.ego(),
+      w = {
+        left: 0,
+        top: 0,
+        columns: settings.window_columns,
+        rows: settings.window_rows
+      };
+
   if (typeof ego !== 'undefined') {
     w.left = clamp(
       ego.position[1] - Math.floor(settings.window_columns / 2),
@@ -646,7 +649,7 @@ function onDonationProcessed(msg) {
     recipient_name = 'you';
   } else if (recipient_id === 'all') {
     recipient_name = 'all players';
-  } else if (recipient_id.indexOf('group:') == 0) {
+  } else if (recipient_id.indexOf('group:') === 0) {
     team_idx = +recipient_id.substring(6);
     recipient_name = 'all ' + Object.keys(PLAYER_COLORS)[team_idx] + ' players';
   } else {
@@ -862,9 +865,9 @@ $(document).ready(function() {
       return;
     }
 
-    if (settings.donation_type == 'individual') {
+    if (settings.donation_type === 'individual') {
       recipient_id = recipient.id;
-    } else if (settings.donation_type == 'group') {
+    } else if (settings.donation_type === 'group') {
       recipient_id = 'group:' +  color2idx(recipient.color).toString();
     } else {
       return;
@@ -892,7 +895,7 @@ $(document).ready(function() {
       amount: amt
     };
     socket.send(msg);
-  }
+  };
 
   var pixels2cells = function(pix) {
     return Math.floor(pix / (settings.block_size + settings.padding));
@@ -902,7 +905,7 @@ $(document).ready(function() {
     var colors = Object.values(PLAYER_COLORS);
     var value = color.join(',');
     for (var idx=0; idx < colors.length; idx++) {
-      if (colors[idx].join(',') == value) {
+      if (colors[idx].join(',') === value) {
         return idx;
       }
     }
