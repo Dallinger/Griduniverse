68b6bbdad09a312560a5303f96315130e84f3445
Alec Mitchell <alecpm@gmail.com>
2017-10-11 22:10:40 -0700

    Fix recruitment close call. Update waiting room to use new implementation.

--- dlgr/griduniverse/static/scripts/waiting.js
+++ dlgr/griduniverse/static/scripts/waiting.js
@@ -3,6 +3,11 @@
 $(document).ready(function() {
     // wait for participant to be created and quorum to be reached
     dallinger.createParticipant().done(function () {
-        dallinger.goToPage("grid");
+        if (skip_experiment) {
+            $('.main_div').html('<p>The experiment has exceeded the maximum number of participants, your participation is not required. Click the button below to complete the HIT. You will be compensated as if you had completed the task.</p><button type="button" class="button btn-success">Complete</button>')
+            $('.main_div button').on('click', submit_assignment);
+        } else {
+            dallinger.goToPage("grid");
+        }
     });
 });
