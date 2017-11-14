7fae7cea33780f54d4a19a84e228845f67954f5f
Jesse Snyder <jsnyder@wesleyan.edu>
2017-11-14 12:16:13 -0800

    Don't disable donation entirely if alternate_consumption_donation is False

--- dlgr/griduniverse/experiment.py
+++ dlgr/griduniverse/experiment.py
@@ -325,22 +325,56 @@ class Gridworld(object):
         return max(0, raw_remaining)
 
     @property
-    def donation_active(self):
-        """Donation is enabled on even-numbered rounds if
-        alternate_consumption_donation is set to True.
-        """
-        return bool(self.alternate_consumption_donation and self.round % 2)
-
-    @property
     def group_donation_enabled(self):
         return self.donation_group or self.donation_ingroup
+
+    @property
+    def donation_enabled(self):
+        return (
+            (
+                self.group_donation_enabled or
+                self.donation_individual or
+                self.donation_public
+            ) and bool(self.donation_amount)
+        )
+
+    @property
+    def is_even_round(self):
+        return bool(self.round % 2)
+
+    @property
+    def donation_active(self):
+        """Donation is enabled if:
+        1. at least one of the donation_individual, donation_group and
+           donation_public flags is set to True
+        2. donation_amount to some non-zero value
+
+        Further, donation is limited to even-numbered rounds if
+        alternate_consumption_donation is set to True.
+        """
+        if not self.donation_enabled:
+            return False
+
+        if self.alternate_consumption_donation:
+            return self.is_even_round
+
+        return True
+
+    @property
+    def movement_enabled(self):
+        """If we're alternating consumption and donation, Players can only move
+        during consumption rounds.
+        """
+        if self.alternate_consumption_donation and self.donation_active:
+            return False
+        return True
 
     @property
     def consumption_active(self):
         """Food consumption is enabled on odd-numbered rounds if
         alternate_consumption_donation is set to True.
         """
-        return bool(not self.alternate_consumption_donation or not self.round % 2)
+        return not self.alternate_consumption_donation or not self.is_even_round
 
     def check_round_completion(self):
         if not self.game_started:
@@ -787,8 +821,7 @@ class Player(object):
     def move(self, direction, tremble_rate=0):
         """Move the player."""
 
-        # no motion during alternate donation rounds
-        if self.grid.donation_active:
+        if not self.grid.movement_enabled:
             return
 
         if random.random() < tremble_rate:
