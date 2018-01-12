6aae7f5865766e3aa15a48fca4beb7b8bd280573
vlall <vishal.h.lall@gmail.com>
2018-01-12 10:09:23 -0800

    Add variables

--- demos/loop_exp.py
+++ demos/loop_exp.py
@@ -1,16 +1,19 @@
 from dallinger.experiments import Griduniverse
 
 experiment = Griduniverse()
-participants = 2
-iterations = 10
+participants = 1
+iterations = 2
 
 for i in xrange(iterations):
     data = experiment.run(
-        time_per_round = 5.0,
-        mode=u'debug',
+        time_per_round = 100.0,
+        mode=u'sandbox',
         max_participants=participants,
         num_dynos_worker=participants,
     )
 
     results = experiment.analyze(data)
     print(results)
+
+print("Script successfully ran with %d participants for %d iterations" % (participants, iterations))
+
