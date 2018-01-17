84d1c1ecffcf6756816408bf31fa18bf112d6458
Jesse Snyder <jsnyder@wesleyan.edu>
2018-01-17 15:43:16 -0800

    Extract maze generation and pruning for easier testing

--- dlgr/griduniverse/experiment.py
+++ dlgr/griduniverse/experiment.py
@@ -2,7 +2,6 @@
 
 import flask
 import gevent
-import itertools
 import json
 import logging
 import math
@@ -26,6 +25,7 @@ from dallinger.config import get_config
 from dallinger.experiment import Experiment
 from dallinger.heroku.worker import conn as redis
 
+import maze
 from bots import Bot
 from models import Event
 
@@ -992,129 +992,15 @@ class Player(object):
 
 
 class Labyrinth(object):
-    """A maze generator."""
+    """Generates a maze in the form of a matrix of Walls"""
     def __init__(self, columns=25, rows=25, density=1.0, contiguity=1.0):
         if density:
-            walls = self._generate_maze(rows, columns)
+            walls = [Wall(position=pos) for pos in maze.generate(rows, columns)]
             # Add sleep to avoid timeouts
             gevent.sleep(0.00001)
-            self.walls = self._prune(walls, density, contiguity)
+            self.walls = maze.prune(walls, density, contiguity)
         else:
             self.walls = []
-
-    def _generate_maze(self, rows, columns):
-
-        c = (columns - 1) / 2
-        r = (rows - 1) / 2
-
-        visited = [[0] * c + [1] for _ in range(r)] + [[1] * (c + 1)]
-        ver = [["* "] * c + ['*'] for _ in range(r)] + [[]]
-        hor = [["**"] * c + ['*'] for _ in range(r + 1)]
-
-        sx = random.randrange(c)
-        sy = random.randrange(r)
-        visited[sy][sx] = 1
-        stack = [(sx, sy)]
-        while len(stack) > 0:
-            (x, y) = stack.pop()
-            d = [
-                (x - 1, y),
-                (x, y + 1),
-                (x + 1, y),
-                (x, y - 1)
-            ]
-            random.shuffle(d)
-            for (xx, yy) in d:
-                if visited[yy][xx]:
-                    continue
-                if xx == x:
-                    hor[max(y, yy)][x] = "* "
-                if yy == y:
-                    ver[y][max(x, xx)] = "  "
-                stack.append((xx, yy))
-                visited[yy][xx] = 1
-
-        # Convert the maze to a list of wall cell positions.
-        the_rows = ([j for i in zip(hor, ver) for j in i])
-        the_rows = [list("".join(j)) for j in the_rows]
-        maze = [item == '*' for sublist in the_rows for item in sublist]
-        walls = []
-        for idx, value in enumerate(maze):
-            if value:
-                walls.append(Wall(position=[idx / columns, idx % columns]))
-
-        return walls
-
-    def _prune(self, walls, density, contiguity):
-        """Prune walls to a labyrinth with the given density and contiguity."""
-        num_to_prune = int(round(len(walls) * (1 - density)))
-        num_pruned = 0
-        count = 0
-        while num_pruned < num_to_prune:
-            count += 1
-            to_prune = self._classify_terminals(
-                walls, limit=num_to_prune - num_pruned
-            )
-            walls = [w for i, w in enumerate(walls) if i not in to_prune]
-            if len(to_prune) == 0:
-                break
-            num_pruned += len(to_prune)
-
-        num_to_prune = int(round(len(walls) * (1 - contiguity)))
-        to_prune = set(random.sample(range(len(walls)), num_to_prune))
-        walls = [w for i, w in enumerate(walls) if i not in to_prune]
-
-        return walls
-
-    def _classify_terminals(self, walls, limit=None):
-        found = [set()]
-        unmatched = {}
-        enumerated_walls = tuple(enumerate(walls))
-        position_map = {tuple(w.position): i for i, w in enumerated_walls}
-
-        for i, w in enumerated_walls:
-            neighbors = set()
-            for adj in ([1, 0], [-1, 0], [0, 1], [0, -1]):
-                neighbor = tuple(p1 + p2 for p1, p2 in zip(w.position, adj))
-                if neighbor in position_map:
-                    neighbors.add(neighbor)
-            if len(neighbors) <= 1:
-                found[0].add(i)
-                if limit and len(found[0]) >= limit:
-                    break
-            elif len(neighbors) == 2:
-                n_indexes = {position_map[i] for i in neighbors}
-                j = 0
-                while j < len(found):
-                    if n_indexes.intersection(found[j]):
-                        if len(found) < j + 2:
-                            found.append(set())
-                        up_next = found[j + 1]
-                        up_next.add(i)
-                        break
-                    j += 1
-                else:
-                    unmatched[i] = n_indexes
-
-        for index in unmatched:
-            j = 0
-            n_indexes = unmatched[index]
-            while j < len(found):
-                if n_indexes.intersection(found[j]):
-                    if len(found) < j + 2:
-                        found.append(set())
-                    up_next = found[j + 1]
-                    up_next.add(index)
-                    break
-                j += 1
-
-        to_prune = set()
-        count = 0
-        for entries in found:
-            count += 1
-            if len(to_prune) < limit:
-                to_prune = to_prune.union(set(itertools.islice(entries, limit - len(to_prune))))
-        return to_prune
 
 
 def fermi(beta, p1, p2):
