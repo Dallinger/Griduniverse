dc616053e19692c8718971ed41cd9caeaaddc410
Jesse Snyder <jsnyder@wesleyan.edu>
2018-01-17 14:32:59 -0800

    Labyrinth tests

---
+++ test/test_labyrinth.py
@@ -0,0 +1,87 @@
+import pytest
+
+
+class TestLabyrinth(object):
+
+    @pytest.fixture
+    def factory(self, **kw):
+        from dlgr.griduniverse.experiment import Labyrinth
+        return Labyrinth
+
+    def test_defaults_to_25x25(self, factory):
+        labyrinth = factory()
+        maxX = max([wall.position[0] for wall in labyrinth.walls])
+        maxY = max([wall.position[1] for wall in labyrinth.walls])
+        assert maxX == maxY == 24  # zero-index
+
+    def test_zero_density_includes_no_walls(self, factory):
+        labyrinth = factory(columns=4, rows=4, density=0.0)
+        assert len(labyrinth.walls) == 0
+
+    def test_full_density_results_in_50_percent_wall(self, factory):
+        labyrinth = factory(columns=10, rows=10)
+        assert len(labyrinth.walls) == 50
+
+    def test_reducing_density_reduces_wall_count_correspondingly(self, factory):
+        labyrinth = factory(columns=10, rows=10, density=0.5)
+        assert len(labyrinth.walls) == 25
+
+    def test_reducing_density_and_contiguity_reduces_wall_count_by_sum(self, factory):
+        labyrinth = factory(columns=10, rows=10, density=0.5, contiguity=0.5)
+        assert len(labyrinth.walls) == 12  # 50 * .5. * .5, rounded down.
+
+    def test_prune_removes_isolated_walls_only_when_contiguity_is_1(self, factory):
+        """Before:
+        [
+                                                                [0, 7], [0, 8],
+                                                                [1, 7], [1, 8],
+        [2, 0],                 [2, 3],                 [2, 6], [2, 7],
+                [3, 1],                         [3, 5], [3, 6],                 [3, 9],
+        [4, 0],         [4, 2],         [4, 4], [4, 5],
+                                [5, 3], [5, 4],                 [5, 7],
+                [6, 1], [6, 2], [6, 3],
+                [7, 1], [7, 2], [7, 3],
+        [8, 0]
+        ]
+
+        After:
+        [
+                                                                 [0, 7], [0, 8],
+                                                                 [1, 7], [1, 8],
+                                                         [2, 6], [2, 7],
+                                                 [3, 5], [3, 6],
+                                         [4, 4], [4, 5],
+                                 [5, 3], [5, 4],
+                 [6, 1], [6, 2], [6, 3],
+                 [7, 1], [7, 2], [7, 3]
+        ]
+
+        """
+        from dlgr.griduniverse.experiment import Wall
+        labyrinth = factory(columns=10, rows=10)
+        postions = [
+            [0, 7], [0, 8],
+            [1, 7], [1, 8],
+            [2, 0], [2, 3], [2, 6], [2, 7],
+            [3, 1], [3, 5], [3, 6], [3, 9],
+            [4, 0], [4, 2], [4, 4], [4, 5],
+            [5, 3], [5, 4], [5, 7],
+            [6, 1], [6, 2], [6, 3],
+            [7, 1], [7, 2], [7, 3],
+            [8, 0]
+        ]
+        walls = [Wall(position=pos) for pos in postions]
+        pruned = [
+            w.position for w in labyrinth._prune(walls, density=0.5, contiguity=1.0)
+        ]
+        singles_removed = [
+            [0, 7], [0, 8],
+            [1, 7], [1, 8],
+            [2, 6], [2, 7],
+            [3, 5], [3, 6],
+            [4, 4], [4, 5],
+            [5, 3], [5, 4],
+            [6, 1], [6, 2], [6, 3],
+            [7, 1], [7, 2], [7, 3]
+        ]
+        assert pruned == singles_removed
