6f2ec758555c1af4ad1298fa6404b36393faaab4
Carlos de la Guardia <cguardia@yahoo.com>
2018-02-05 01:41:35 -0600

    add new probabilty distributions

--- dlgr/griduniverse/experiment.py
+++ dlgr/griduniverse/experiment.py
@@ -5,6 +5,7 @@ import gevent
 import json
 import logging
 import math
+import numpy
 import random
 import string
 import time
@@ -776,8 +777,84 @@ class Gridworld(object):
 
     def _random_probability_distribution(self, *args):
         """A probability distribution function always returns a [row, column] pair."""
-        row = random.randint(0, self.rows -1)
-        column = random.randint(0, self.columns -1)
+        row = random.randint(0, self.rows - 1)
+        column = random.randint(0, self.columns - 1)
+        return [row, column]
+
+    def _sinusoidal_probability_distribution(self, *args):
+        rows = self.rows
+        cols = self.columns
+        frequency = 10
+        if len(args):
+            try:
+                frequency = int(args[0])
+            except ValueError:
+                pass
+        grid = numpy.tile(numpy.linspace(0, 1, cols), (rows, 1))
+        p = 0.5 + 0.5 * numpy.sin(frequency * grid)
+        p = p / numpy.sum(p)
+        value = choice(rows * cols, p=p.flatten())
+        row = value / cols
+        column = value - (row * cols)
+        return [row, column]
+
+    def _standing_wave_probability_distribution(self, *args):
+        # https://ocefpaf.github.io/python4oceanographers/blog/2013/11/25/waves/
+        rows = self.rows
+        cols = self.columns
+        a = 1
+        if len(args):
+            try:
+                a = int(args[0])
+            except ValueError:
+                pass
+        d = 1
+        t = 20
+        w = 2 * numpy.pi / t
+        k = w**2 / 9.81
+        x = numpy.arange(rows * cols)
+        a1 = (a**2 + a**2 + 2*a * a * numpy.cos(2 * k * x + d))**(0.5)
+        a2 = a * numpy.cos(k * x) + a * numpy.cos(k * x + d)
+        a3 = a * numpy.sin(k * x) - a * numpy.sin(k * x + d)
+        g = numpy.arctan2(a3, a2)
+        p = a1 * numpy.cos(w * x - g)
+        p = (p-numpy.min(p)) / (numpy.max(p) - numpy.min(p))
+        p = p / numpy.sum(p)
+        value = choice(rows * cols, p=p)
+        row = value / cols
+        column = value - (row * cols)
+        return [row, column]
+
+    def _gaussian_mixture_probability_distribution(self, *args):
+        # Not implemented yet, return simple random value
+        rows = self.rows
+        cols = self.columns
+        row = random.randint(0, rows - 1)
+        column = random.randint(0, cols - 1)
+        return [row, column]
+
+    def _horizontal_gradient_probability_distribution(self, *args):
+        rows = self.rows
+        cols = self.columns
+        cells = rows * cols
+        grid = numpy.gradient(cells * numpy.random.random((rows, cols)), axis=0)
+        grid = (grid-numpy.min(grid)) / (numpy.max(grid) - numpy.min(grid))
+        grid = grid / numpy.sum(grid)
+        value = choice(cells, p=grid.flatten())
+        row = value / cols
+        column = value - (row * cols)
+        return [row, column]
+
+    def _vertical_gradient_probability_distribution(self, *args):
+        rows = self.rows
+        cols = self.columns
+        cells = rows * cols
+        grid = numpy.gradient(cells * numpy.random.random((rows, cols)), axis=1)
+        grid = (grid-numpy.min(grid)) / (numpy.max(grid) - numpy.min(grid))
+        grid = grid / numpy.sum(grid)
+        value = choice(cells, p=grid.flatten())
+        row = value / cols
+        column = value - (row * cols)
         return [row, column]
 
     def _edge_bias_probability_distribution(self, *args):
@@ -796,7 +873,7 @@ class Gridworld(object):
         total = sum(values)
         for index, value in enumerate(values):
             values[index] = float(value)/float(total)
-        value = choice(rows * cols, p = values)
+        value = choice(rows * cols, p=values)
         row = value / cols
         column = value - (row * cols)
         return [row, column]
@@ -817,7 +894,7 @@ class Gridworld(object):
         total = sum(values)
         for index, value in enumerate(values):
             values[index] = float(value)/float(total)
-        value = choice(rows * cols, p = values)
+        value = choice(rows * cols, p=values)
         row = value / cols
         column = value - (row * cols)
         return [row, column]
