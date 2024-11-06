2fa2039b28b8df54efd9267fa4e18ee5295a1718
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2024-11-06 20:19:39 +0100

    Compile JS bundle files

--- dlgr/griduniverse/static/scripts/dist/difi.js
+++ dlgr/griduniverse/static/scripts/dist/difi.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 18);
+/******/ 	return __webpack_require__(__webpack_require__.s = 19);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -1031,7 +1031,7 @@ module.exports = function (css) {
 
 /***/ }),
 
-/***/ 18:
+/***/ 19:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
